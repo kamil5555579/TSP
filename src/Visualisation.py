1ed3bc71b07022b55ff2b02cd08febd8c8b53899
Tymon <01169766@pw.edu.pl>
2023-12-14 20:14:48 +0100

    Adding GIF's and pmx
    Png's to create GIF are created in tmp_figures folder. Also I added pmx crossover method. Last small change is adding progressbar to simulating populations

--- src/Visualisation.py
+++ src/Visualisation.py
@@ -1,79 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import imageio.v2 as imageio
+import os
 
 
-def visualise_Map(
-    city_coordinates, 
-    size, 
-    filename = "fig.png", 
-    show = False,
-    title = ""):
+def make_gif(img_path, num_generations, gif_path="generations.gif"):#os.getcwd()+"/generations.gif"):
+    # Parameters
+    # display_time time after gif will loop in seconds
+    display_time = 30
 
-    fig, ax = plt.subplots()
-    ax.plot(np.append(city_coordinates[0, :], city_coordinates[0, 0]),
-     np.append(city_coordinates[1, :], city_coordinates[1, 0]), '-o')
-    ax.set_xlim(0,size)
-    ax.set_ylim(0,size)
-    if title != "":
-        ax.set_title('Generation: ' + str(title) )
-    
-    fig.savefig(filename)
-    if show is True:
-        plt.show()
-
-
-def make_gif(
-    list_of_cisty_coordinates_to_plot, 
-    size, 
-    img_path, 
-    gif_name = 'nextGeneration.gif',
-    list_of_generation_number = None):
-
-    i = 0
     filenames = []
+    for j in range(num_generations):
+        filenames.append('fig'+str(j)+'.png')
 
-    for coordinates in list_of_cisty_coordinates_to_plot:
-        file_name = img_path + f"/figure_{i}.png"
-        filenames.append(file_name)
-        # Optional use of generation number
-        if list_of_generation_number == None:
-            visualise_Map(
-                coordinates, 
-                size, 
-                filename = file_name)
-        else:
-            visualise_Map(
-                coordinates, 
-                size, 
-                filename = file_name, 
-                title = list_of_generation_number[i])
-        i += 1
-
-    print(filenames)
-    with imageio.get_writer(gif_name, fps = 0.5, loop = 10) as writer:
+    os.chdir(img_path)
+    with imageio.get_writer(gif_path, fps = num_generations/display_time, loop = 10) as writer:
         for filename in filenames:
             image = imageio.imread(filename)
             writer.append_data(image)
-
-
-# Example of make_gif() usage:
-'''
-size = 10
-list_of_cisty_coordinates_to_plot = []
-img_path = r"/home/tymon/Desktop/Tutorials/GeneticAlgorithm's/images"
-
-testArray = np.array([[1, 3, 5, 5, 7, 9, 9, 1], [6, 6, 4, 6, 6, 6, 2, 2]])
-# Using np.copy instead of reference (later changes doesn't influence this list)
-list_of_cisty_coordinates_to_plot.append( np.copy(testArray) )
-
-# Swaping columns
-testArray[:, [2, 3]] = testArray[:, [3, 2]]
-list_of_cisty_coordinates_to_plot.append( np.copy(testArray) )
-
-make_gif(
-    list_of_cisty_coordinates_to_plot, 
-    size, 
-    img_path,
-    list_of_generation_number = [1, 100])
-'''
+    os.chdir("..")
